ff19239bdae6cef67f5a7cbd5ecd240dc2ba4e05
Juanjo Daza <juanjod54@gmail.com>
2021-04-11 13:33:16 +0200

    Update

--- synestesia/zip_libraries.sh
+++ synestesia/zip_libraries.sh
@@ -13,10 +13,20 @@ then
 	sudo apt-get install zip
 fi
 
+echo "Zip main libraries"
 for lib in main/*
 do
 	echo "$(basename $lib)" 
 	zip -j libraries/"$(basename $lib).zip" $lib/*
 done
+
+if [ -d "module" ]
+	echo "Zip module libraries"
+	for lib in module/*
+	do
+		echo "$(basename $lib)" 
+		zip -j libraries/"$(basename $lib).zip" $lib/*
+	done
+fi
 
 echo "\nLibraries have been updated!\n"
