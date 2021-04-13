d3809336f6150bf9b2dedda46bc00f06ec36dca9
Juanjo Daza <juanjod54@gmail.com>
2021-04-13 22:39:58 +0200

    Minor changes
    Some refactors

--- synestesia/test/SYN-8-file-manager/SYN-8-file-manager.ino
+++ synestesia/test/SYN-8-file-manager/SYN-8-file-manager.ino
@@ -5,13 +5,26 @@ char * content;
 
 void setup() {
   Serial.begin(9600);
-  init_file_manager(false);
+  while(! Serial){delay(500);}
   
-  while(! Serial);
-  Serial.println("Start");
+  init_file_manager();
+  
+  Serial.println("Configuration load has finished");
 
+  Serial.println("Preparing to read global.syn configuration file:");
   content = read_from_file("/global.syn");
   Serial.write(content);
+  Serial.println("");
+  
+  Serial.println("Preparing to overwrite global.syn configuration file:");
+  write_to_file("/global.syn", "New configuration values ... ");
+
+  Serial.println("Preparing to read global.syn configuration file after overwrite:");
+  content = read_from_file("/global.syn");
+  Serial.write(content);
+  Serial.println("");
+  
+  end_file_manager();
 }
 
 void loop() {
