758c55851ca6cdefe1d7b4c56db039c4d7fd3eb0
Juanjo Daza <juanjod54@gmail.com>
2021-06-14 00:48:23 +0200

    Removed serial.println

--- synestesia/libs/main/PostUp/postUp.cpp
+++ synestesia/libs/main/PostUp/postUp.cpp
@@ -18,7 +18,6 @@ void start_postUp_service() {
 }
 
 void send_package(char * msg, IPAddress address) {
-    Serial.println(msg);
     postUpService.beginPacket(address, PORT);
     postUpService.write(msg);
     postUpService.endPacket();
@@ -32,6 +31,5 @@ float receive_udp_freq() {
     for (i = 0; i < BUFF_SIZE; i++) { buff[i] = '\0'; }
 
     postUpService.read((char *) buff, BUFF_SIZE);
-    Serial.println(buff);
     return String(buff).toFloat();
 }
